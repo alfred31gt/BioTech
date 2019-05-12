3aa0878839ecca0e41e1a6a098794b1945facbd3
alfred31gt <fredgo3199@gmail.com>
2019-05-12 11:57:43 -0600

    version8.1

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,29 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'El papel en el desarrollo de la vida',
+    lastText: '',
+    face: 'imagenes/img3.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Un elemento esencial, y protagonista del origen de la vida',
+    lastText: '',
+    face: 'imagenes/img4.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'El agua puede transformar nuestra vida cotidiana',
+    lastText: '',
+    face: 'imagenes/img5.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'responsabilidad del ser humano',
+    lastText: '',
+    face: 'imagenes/img6.jpg'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'El agua y la vida',
+    lastText: '',
+    face: 'imagenes/img7.jpg'
   }];
 
   return {
